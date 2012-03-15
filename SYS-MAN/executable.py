b02ffae2be7f4a3e0c201ac81170a9f8350b1fda
Fabrizio Rosa <fabrizio.rosa@gmail.com>
2012-03-15 21:05:42 +0100

    Add methods to Executables Class

--- SYS-MAN/executable.py
+++ SYS-MAN/executable.py
@@ -12,18 +12,30 @@ class Executable():
         
     '''
 
-    def __init__(self, triliteral, execname, version):
+    def __init__(self, execname, path):
         '''
         Constructor
         '''
-        if len(triliteral) == 3:
-            self.triliteral = triliteral
-        else:
-            raise NameLengthError(triliteral, 'name must be 3 character')
+#        if len(triliteral) == 3:
+#            self.triliteral = triliteral
+#        else:
+#            raise NameLengthError(triliteral, 'name must be 3 character')
         self.filename = execname
+        self.location = path
+        # controllare la presenza del file nel path
+        # se l'eseguibile non viene trovato chiamare l'eccezione
 
-    def get_local_version(self):
+    def get_version(self):
         pass
     
-    def get_remote_version(self):
+    def get_mda5(self):
+        pass
+    
+    def get_sha1(self):
+        pass
+ 
+    def get_privileges(self):
+        pass
+    
+    def get_owner(self):
         pass
