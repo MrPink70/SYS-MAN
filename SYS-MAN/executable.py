443317fa0537e5dd8fbf7f6581f5e48c42c10983
Fabrizio Rosa <fabrizio.rosa@gmail.com>
2012-03-18 16:41:34 +0100

    Add executable module functions body

--- SYS-MAN/executable.py
+++ SYS-MAN/executable.py
@@ -5,6 +5,8 @@ Created on 07/mar/2012
 '''
 
 from smerrors import *
+from subprocess import check_output
+import os
 class Executable():
     '''A executable representation
     
@@ -22,20 +24,45 @@ class Executable():
 #            raise NameLengthError(triliteral, 'name must be 3 character')
         self.filename = execname
         self.location = path
+        self.fullname = path + os.sep + execname
         # controllare la presenza del file nel path
         # se l'eseguibile non viene trovato chiamare l'eccezione
 
-    def get_version(self):
-        pass
+    def get_CSCI_version(self):
+        fullversion = check_output(['/usr/bin/ident', self.fullname]).splitlines()
+        version = ''
+        for line in fullversion:
+            if line.find('CSCI') > -1: version = version + line.lstrip() +'\n'
+        return version.rstrip('\n')
     
-    def get_mda5(self):
-        pass
+    def get_EXEC_version(self):
+        fullversion = self.get_CSCI_version().splitlines()
+        version = ''
+        for line in fullversion:
+            if line.find(self.filename[0:8]) > -1: version = version + line + '\n'
+        return version.rstrip('\n')
+    
+    def get_md5(self):
+        md5code = check_output(['/usr/bin/md5sum', self.fullname])
+        return md5code.rstrip('\n')
     
     def get_sha1(self):
-        pass
+        sha1code = check_output(['/usr/bin/sha1sum', self.fullname])
+        return sha1code.rstrip('\n')
  
     def get_privileges(self):
-        pass
+        statexecfile = os.stat(self.fullname).st_mode
+        return statexecfile
     
     def get_owner(self):
-        pass
+        statexecfile = os.stat(self.fullname).st_uid
+        return statexecfile
+    
+if __name__ == '__main__':
+    exfile = Executable('E000XTH0-01RFCRHEL3', '/home/fabrizio')
+    print 'fullversion :\n' + exfile.get_CSCI_version()
+    print 'versione    :\n' + exfile.get_EXEC_version()
+    print 'md5         :\n' + exfile.get_md5()
+    print 'sha1        :\n' + exfile.get_sha1()
+    print 'privilegi   :\n' + oct(exfile.get_privileges())
+    print 'proprietario:\n' + str(exfile.get_owner())
